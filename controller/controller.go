ac64a58849fdd9ebb27368c1eabc9b7a1701354d
Matt Pitts <matt@gopitts.net>
2024-10-03 08:37:57 -0400

    cors support

--- controller/controller.go
+++ controller/controller.go
@@ -21,6 +21,9 @@ func New(browser *rod.Browser) *Controller {
 }
 
 func (r *Controller) GetLinkedInProfile(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", req.Header.Get("Origin"))
+	w.Header().Set("Access-Control-Allow-Methods", "GET,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Accept")
 	profile, err := r.getProfile()
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
--- routes/routes.go
+++ routes/routes.go
@@ -15,6 +15,13 @@ func AppRoutes(browser *rod.Browser) *mux.Router {
 	//Other Routes
 	contInst := controller.New(browser)
 	router.HandleFunc("/api/linkedin/profile", contInst.GetLinkedInProfile).Methods(http.MethodGet)
+	router.HandleFunc("/api/linkedin/profile", ProvideOptions).Methods(http.MethodOptions)
 
 	return router
+}
+
+func ProvideOptions(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", req.Header.Get("Origin"))
+	w.Header().Set("Access-Control-Allow-Methods", "GET,OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Accept")
 }
